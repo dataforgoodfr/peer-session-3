9542188392d3109b4b7411a1adb46ff00fd5dae8
gmguarino <gmguarino1@gmail.com>
2024-02-28 19:25:02 +0100

    change env vars

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ server_port = 8081
 logger.debug("Got the following environment variables:")
 logger.debug(f"DATABASE_HOST: {os.environ.get('DATABASE_HOST')}")
 logger.debug(f"DATABASE_PORT: {os.environ.get('DATABASE_PORT')}")
-logger.debug(f"DATABASE_NAME: {os.environ.get('DATABASE_NAME')}")
-logger.debug(f"DATABASE_USER: {os.environ.get('DATABASE_USER')}")
-logger.debug(f"DATABASE_PASSWORD: {os.environ.get('DATABASE_PASSWORD')}")
+logger.debug(f"POSTGRES_DB: {os.environ.get('POSTGRES_DB')}")
+logger.debug(f"POSTGRES_USER: {os.environ.get('POSTGRES_USER')}")
+logger.debug(f"POSTGRES_PASSWORD: {os.environ.get('POSTGRES_PASSWORD')}")
 
 if __name__ == "__main__":
     logger.info("Starting the Webserver")
